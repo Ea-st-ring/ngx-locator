2945e3fee019c3462f2daf0a7978b227f0b3e70f
donghyun <antepost24@soongsil.ac.kr>
2026-02-04 15:39:34 +0900

    feat: set prettier lint

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2020: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  ignorePatterns: ['dist/', 'node_modules/', '.npm-cache/', '*.tgz'],
+};
